f61ed5f6c9ea702a44b55bd8c7e53e1f0b0cb0fa
Handshape <jturner@CSPS002478.csps-efpc.com>
2024-03-06 10:27:15 -0500

    Restore time information to general prompts.

--- app.py
+++ app.py
@@ -145,13 +145,14 @@ def gpt_socket(personality):
             chat_session = '';
             if(not lock.acquire(blocking=False)):
                 print("Blocking for pre-parsing lock")
+                ws.send("(Currently helping another user...)\n")
                 lock.acquire()
             if((url is not None) or (text is not None)) :
                 chat_session += rag.get_rag_prefix(personality, url, rag_text = text, rag_prefix = rag_source_description, system_prefix=system_prefix, system_suffix=system_suffix)
             else :
                 chat_session += rag.get_personality_prefix(personality, system_prefix=system_prefix, system_suffix=system_suffix) + prompt_prefix
             # At this stage, we're positioned just before the prompt.
-            chat_session += message + prompt_suffix + response_prefix;
+            chat_session += time_prompt + message + prompt_suffix + response_prefix;
             print(chat_session)
             llm.reset()
             while True:
